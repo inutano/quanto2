577a7df3ff76f6d128b133dc5c7396c94766f8eb
Tazro Inutano Ohta <inutano@gmail.com>
2019-10-04 11:59:39 +0900

    fixed conf path

--- sh/run.sh
+++ sh/run.sh
@@ -2,15 +2,14 @@
 set -eux
 
 abspath(){
-  p=${1}
-  cd $(dirname ${p}) && pwd -P
+  cd $(dirname ${1}) && pwd -P
 }
 
 BASE_DIR=$(abspath ${0})
 DATA_DIR=$(abspath ${1})
 DEST_DIR=$(abspath ${2})
 CWL_DIR="${BASE_DIR}/../cwl"
-JOB_CONF="${BASE_DIR}/array.conf"
+JOB_CONF="${DEST_DIR}/array.conf"
 
 # Create array job configuration
 find ${DATA_DIR} -name '*fastq*' > ${JOB_CONF}
