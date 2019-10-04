2608143b418e0695038fb28769ad098b5afec825
Tazro Inutano Ohta <inutano@gmail.com>
2019-10-04 14:35:55 +0900

    fixed wrong path, check if conf exists

--- sh/run.sh
+++ sh/run.sh
@@ -7,12 +7,14 @@ abspath(){
 
 BASE_DIR=$(abspath ${0})
 DATA_DIR=$(abspath ${1})/$(basename ${1})
-DEST_DIR=$(abspath ${2})/$(basename ${1})
+DEST_DIR=$(abspath ${2})/$(basename ${2})
 CWL_DIR="${BASE_DIR}/../cwl"
 JOB_CONF="${DEST_DIR}/array.conf"
 
 # Create array job configuration
-find ${DATA_DIR} -name '*.fastq.*' > ${JOB_CONF}
+if [[ ! -e ${JOB_CONF} ]]; then
+  find ${DATA_DIR} -name '*.fastq.*' > ${JOB_CONF}
+fi
 
 # Execute array job
 source "/home/geadmin/UGED/uged/common/settings.sh"
